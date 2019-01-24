c7d71a43b81b2296a79755f85950e9199ef2e94d
LulaEcstasy <42184824+LulaEcstasy@users.noreply.github.com>
2019-01-24 20:59:59 +0100

    Create testfckgsm.sh

---
+++ testfckgsm.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+sudo dpkg --add-architecture i386; sudo apt update; sudo apt install mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 libstdc++6:i386
